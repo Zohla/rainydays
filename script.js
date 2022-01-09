f09b240ef05e8a9ed71e4dcd6bd87db97be81ad5
Maren Lilleberre <marenlilleberre@gmail.com>
2022-01-09 14:00:37 +0100

    add error msg if size not chosen and add confirmation for item added to shopping bag

--- script.js
+++ script.js
@@ -4,11 +4,28 @@ const jacketSizeSelect = document.getElementById("size");
 
 const jacketSize = jacketSizeSelect.value;
 
+const cartConfMessage = document.querySelector(".cart-add-conf-msg");
+const sizeErrorMsg = document.querySelector(".size-error-msg");
+
+let pickedSize;
+
 jacketSizeSelect.addEventListener("change", (e) => {
-  let picketSize = e.target.value;
-  console.log(picketSize);
+  pickedSize = e.target.value;
+  /* console.log(pickedSize); */
 });
 
 addToCartBtn.addEventListener("click", (e) => {
-  e.preventDefault();
+  /*  e.preventDefault(); */
+  /* console.log(pickedSize); */
+  /*   cartConfMessage.classList.toggle("hide-msg");
+   */ if (pickedSize !== undefined && pickedSize !== "choose") {
+    console.log(pickedSize);
+    cartConfMessage.classList.remove("hide-msg");
+    sizeErrorMsg.classList.add("hide-msg");
+    /* sizeErrorMsg.classList.remove("hide-msg"); */
+  } else {
+    console.log(pickedSize);
+    cartConfMessage.classList.add("hide-msg");
+    sizeErrorMsg.classList.remove("hide-msg");
+  }
 });
