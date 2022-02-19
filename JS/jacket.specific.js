8ba0dd30a68a44b62a9ac066d13b6c3848827055
Maren Lilleberre <marenlilleberre@gmail.com>
2022-02-19 09:49:46 +0100

    added headless CMS for the products

--- JS/jacket.specific.js
+++ JS/jacket.specific.js
@@ -9,6 +9,46 @@ const goToCheckout = document.querySelector(".go-to-checkout");
 
 let pickedSize;
 
+//get data from headless CMS and output on page
+
+const querystring = document.location.search;
+const params = new URLSearchParams(querystring);
+const productId = params.get("id");
+console.log(productId);
+const productContainer = document.querySelector(".container-jacket-specific");
+const h1JacketName = document.querySelector(".h1-sub-pages");
+const jacketImg = document.querySelector(".jacket-specific-img");
+const jacketDescription = document.querySelector(".product-info");
+const productPrice = document.querySelector(".price");
+const navTree = document.querySelector(".nav-tree");
+const checkoutLink = document.querySelector(".checkout-link");
+
+const url = `http://wp-rainydays.maweb.tech/wp-json/wc/store/products/${productId}`;
+
+console.log(url);
+
+async function getProduct() {
+  try {
+    const response = await fetch(url);
+    const getResults = await response.json();
+    console.log(getResults);
+    createHTML(getResults);
+  } catch (error) {
+    console.log(error);
+    productContainer.innerHTML = "<p>Sorry, an error occured</p>";
+  }
+}
+getProduct();
+
+function createHTML(product) {
+  navTree.innerHTML += `${product.name}`;
+  h1JacketName.innerHTML = `${product.name}`;
+  jacketImg.src = `${product.images[0].src}`;
+  jacketDescription.innerHTML += `${product.description}`;
+  productPrice.innerHTML = `$ ${product.prices.price}`;
+  checkoutLink.href = `checkout.html?id=${product.id}`;
+}
+
 jacketSizeSelect.addEventListener("change", (e) => {
   pickedSize = e.target.value;
 });
---
+++ JS/shop.js
@@ -0,0 +1,27 @@
+const url = "http://wp-rainydays.maweb.tech/wp-json/wc/store/products";
+const productContainer = document.querySelector(".products");
+
+async function getProducts() {
+  try {
+    const response = await fetch(url);
+    const getResults = await response.json();
+    createHTML(getResults);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getProducts();
+
+function createHTML(products) {
+  products.forEach(function (product) {
+    productContainer.innerHTML += `<div class="product">
+    <a href="jacket-specific.html?id=${product.id}">
+    <h2>${product.name}</h2>
+    <img src="${product.images[0].src}" alt="${product.name}">
+    <p class="product_short_description"> ${product.short_description}</p>
+    <p class="price">${product.prices.price} ${product.prices.currency_symbol} </p>
+    </a>
+    </div>`;
+  });
+}
